13a33bab738b0c5076aa7f149d3f162e2dbc5255
Charles Altopiedi <chuckalto@gmail.com>
2021-11-17 21:57:15 -0500

    added a js confirm, imported picture

--- app2.js
+++ app2.js
@@ -1,2 +1,2 @@
 let user_name = prompt("What is your name?");
-document.write(user_name);
+document.write("Hi " + user_name + ", that's a fine name");
---
+++ app4.js
@@ -0,0 +1,9 @@
+let luck = window.confirm("press OK for good luck")
+let picurl = "dog.jpg"
+
+if (luck == true){
+    document.write('<img src="dog.jpg" width="500" height="500"')
+    document.write("Oh boy a lucky dog")
+}
+    
+    
