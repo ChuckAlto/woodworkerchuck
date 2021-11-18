1cebad19bed74d79f231713a0e345ff2b6b3c352
Charles Altopiedi <chuckalto@gmail.com>
2021-11-17 20:43:13 -0500

    added 3 js prompts, added pictures

---
+++ app1.js
@@ -0,0 +1,9 @@
+let woodtype = prompt("What is your favorite type of wood?");
+
+console.log(woodtype);
+
+if (woodtype.toLowerCase() == 'walnut' || woodtype.toLowerCase() == 'cherry' || woodtype.toLowerCase() == 'pine' || woodtype.toLowerCase() == 'cedar' || woodtype.toLowerCase() == 'redwood' || woodtype.toLowerCase() == 'maple'){
+    document.write ("That's a Fine Wood, Welcome! ")
+} else{document.write("To each their own I guess. welcome anyway ")
+}
+
---
+++ app3.js
@@ -0,0 +1,7 @@
+let bench = prompt ("Do You Like My Workbench?");
+console.log(bench);
+
+if (bench.toLowerCase() == "yes"){
+document.write ("Hey, Thanks!")
+} else {document.write ("oh.")
+}
